c32aebfb1c9853247930e90299bf596eb0284a82
STEAM <equiposteamsl@gmail.com>
2024-05-08 08:29:16 -0500

    Ultimos cambios

--- src/app/pipes/moneda.pipe.ts
+++ src/app/pipes/moneda.pipe.ts
@@ -1,12 +1,12 @@
-import { Pipe, PipeTransform } from '@angular/core';
+// import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-  name: 'moneda'
-})
-export class MonedaPipe implements PipeTransform {
+// @Pipe({
+//   name: 'moneda'
+// })
+// export class MonedaPipe implements PipeTransform {
 
-  transform(moneda:number): unknown {
-    return new Intl.NumberFormat("de-DE", { currency: "USD" }).format(moneda);
-  }
+//   transform(moneda:number): unknown {
+//     return new Intl.NumberFormat("de-DE", { currency: "USD" }).format(moneda);
+//   }
 
-}
+// }
