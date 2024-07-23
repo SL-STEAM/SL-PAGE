66be8f9b3bf0bccb1fae28b26b74f2f9f1623e79
SL Consulting (STEM) <equiposteamsl@gmail.com>
2024-07-23 17:29:12 -0500

    Se soluciona error de pollyfills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { HeaderComponent } from './compartido/header/header.component';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
     standalone: true,
     imports: [HeaderComponent, RouterOutlet, FooterComponent]
 })
