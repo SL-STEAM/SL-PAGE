89d8dff8f7400d21b1818f1f36afaed67539bef0
Dashboard <andres18fcr@gmail.com>
2024-07-24 09:52:43 -0500

    SECCIÓN LANDING BUSINESS

--- src/app/paginas/business-s/business-s.component.ts
+++ src/app/paginas/business-s/business-s.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import Aos from 'aos';
 import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common'; // Importa NgIf
 
 @Component({
     selector: 'app-business-s',
     templateUrl: './business-s.component.html',
     styleUrls: ['./business-s.component.scss'],
     standalone: true,
-    imports: [RouterLink]
+    imports: [RouterLink, NgIf] // Agrega NgIf a la lista de imports
 })
 export class BusinessSComponent implements OnInit {
   isPopupVisible = false;
